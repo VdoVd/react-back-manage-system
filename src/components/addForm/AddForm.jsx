65329433793f516df02d613e9ddcd9d404111b5f
hello-express <liuhaobin486@gmail.com>
2022-09-18 13:59:07 +0800

    finish addForm in category

--- src/components/addForm/AddForm.jsx
+++ src/components/addForm/AddForm.jsx
@@ -2,22 +2,28 @@ import {Form, Input, Select} from "antd";
 import {Option} from "antd/es/mentions";
 import Item from "antd/es/list/Item";
 
-const AddForm=()=>{
+const AddForm=(props)=>{
     const [form]=Form.useForm()
+    console.log(props.categories,'categories')
+    props.SetForm(form)
+    console.log(form.getFieldsValue(),'addForm')
     return(
        <Form form={form}>
-           <Form.Item>
-
+           <Form.Item name="Id">
            <Select
                style={{width: "472px"}}
-               placeholder="Select a option and change input text above"
+               placeholder=""
            >
                <Option value='0'>一级分类</Option>
-               <Option value='1'>电脑</Option>
-               <Option value='2'>图书</Option>
+               {
+                   props.categories.map(c=><Option value={c._id}>{c.name}</Option>)
+               }
            </Select>
            </Form.Item>
-           <Form.Item>
+           <Form.Item
+               name="text"
+            initialValue=''
+           >
                <Input placeholder='请输入分类名称'></Input>
            </Form.Item>
        </Form>
--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -2,13 +2,13 @@ import React, {useEffect, useRef, useState} from "react";
 import {Button, Card, Table, message, Modal} from "antd";
 import {ArrowRightOutlined, PlusOutlined} from "@ant-design/icons";
 import LinkButton from "../../components/button/button";
-import {reqCategory, reqUpdateCategory} from "../../api";
+import {reqAddCategory, reqCategory, reqUpdateCategory} from "../../api";
 import AddForm from "../../components/addForm/AddForm";
 import UpdateForm from "../../components/updateForm/UpdateForm";
 
 
 const Category=()=>{
-    const [name,Setname]=useState('')
+    const [name,Setname]=useState('unset')
     const [id,SetId]=useState()
     const [curCategory,SetCurCategory]=useState()
     const [colmuns,SetColmuns]=useState([
@@ -23,6 +23,7 @@ const Category=()=>{
                 <span>
                     <LinkButton onclick={(category)=>{
                         SetId(category._id)
+                        Setname(category.name)
                         showUpdate(category)
                     }}>修改分类</LinkButton>
                     <LinkButton onClick={()=>{showSubCategory(category)
@@ -61,10 +62,12 @@ const Category=()=>{
             const cate=result.data
             if(parentId==='0'){
                 SetCatesgories(cate)
+                console.log('setcategory',categories)
                 // console.log('change category')
             }else {
                 SetSubCategory(cate)
-                console.log(cate)
+                console.log(cate,'subcate')
+                SetCatesgories(cate)
                 // console.log('sub change')
             }
         }else {
@@ -97,14 +100,25 @@ const Category=()=>{
     }
     const showUpdate=(category)=>{
         SetCurCategory(category)
+        Setname(category.name)
         SetShowStatus(2)
         console.log()
     }
     const handleCancel=()=>{
         SetShowStatus(0)
     }
-    const addCategory=()=>{
-        console.log('addCategory()')
+    const addCategory=async ()=>{
+        SetShowStatus(0)
+        const {Id,text}=form.getFieldsValue()
+        form.resetFields()
+        const result=await reqAddCategory(text,Id)
+        if(result.status===0){
+            if(parentId===Id){
+                getCategorys(Id)
+            }else if(parentId==='0'){
+                getCategorys(0)
+            }
+        }
     }
     const updateCategory=async ()=>{
         const id=curCategory._id
@@ -114,6 +128,7 @@ const Category=()=>{
         SetShowStatus(0)
         const result=await reqUpdateCategory({id,text})
         console.log(result,'result')
+        console.log(categories)
         if(result.status===0){
             getCategorys(parentId)
         }
@@ -128,7 +143,9 @@ const Category=()=>{
             width: '300px',
             render:(category)=>(
                 <span>
-                    <LinkButton onClick={()=>{showUpdate(category)}}>修改分类</LinkButton>
+                    <LinkButton onClick={
+                        ()=>{showUpdate(category)}
+                     }>修改分类</LinkButton>
                     <LinkButton onClick={()=>{showSubCategory(category)
                         console.log('linkbutton onclick-----',category)
                     }}>查看子分类</LinkButton>
@@ -140,7 +157,7 @@ const Category=()=>{
         Setname(text)
         console.log('handleInput','name',text)
     }
-    console.log(text,'text')
+    console.log(categories,'up categories')
     return(
         <Card
             title={title}
@@ -154,10 +171,10 @@ const Category=()=>{
                 pagination={{defaultPageSize:5,showQuickJumper:true}}
             />
             <Modal title="添加分类" open={showStatus===1} onOk={addCategory} onCancel={handleCancel}>
-                <AddForm />
+                <AddForm categories={categories} curCategory={curCategory} SetForm={SetForm}/>
             </Modal>
             <Modal title="更新分类" open={showStatus===2} onOk={updateCategory} onCancel={handleCancel}>
-                <UpdateForm  SetForm={SetForm} SetText={SetText}/>
+                <UpdateForm  SetForm={SetForm} SetText={SetText} name={name}/>
             </Modal>
             <Modal title="删除分类" open={false} onOk={handleOk} onCancel={handleCancel}>
             </Modal>
